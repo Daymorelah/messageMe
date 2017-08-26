9a8bbe62cf45229d129b9d77f826c7c416b952bf
Daymorelah <demola.hussainin@gmail.com>
2017-08-26 09:15:18 +0100

    Remove groupUsers model file

--- server/models/groupusers.js
+++
@@ -1,9 +0,0 @@
-
-
-export default (sequelize, DataTypes) => {
-  var GroupUsers = sequelize.define('GroupUsers', {
-    userid: DataTypes.INTEGER,
-    groupid: DataTypes.INTEGER
-  });
-  return GroupUsers;
-};
